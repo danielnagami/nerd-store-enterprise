53823d2092b0c7d76fac7bf30b3ba10356bf2e05
flango <daniel.nagami@woopi.com.br>
2020-12-23 03:54:48 -0300

    Data access

---
+++ src/services/NSE.Catalogo.API/Data/CatalogContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using NSE.Core.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data
+{
+    public class CatalogContext : DbContext, IUnitOfWork
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+
+    }
+}
