e1012edc57b4ae59588f647521af34565c6abd2c
flango <daniel.nagami@woopi.com.br>
2021-01-30 10:45:15 -0300

    Setting entities, value objects and validations

---
+++ src/building blocks/NSE.Core/DomainObjects/Email.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NSE.Core.DomainObjects
+{
+    public class Email
+    {
+        public const int EnderecoMaxLength = 254;
+        public const int EnderecoMinLength = 5;
+        public string Endereco { get; private set; }
+
+        protected Email() { }
+
+        public Email(string endereco)
+        {
+            if (!Validar(endereco)) throw new DomainException("E-mail inválido");
+            Endereco = endereco;
+        }
+        
+        public static bool Validar(string email)
+        {
+            var regexEmail = new Regex(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z");
+            return regexEmail.IsMatch(email);
+        }
+    }
+}
