01c580b97b916b6870188652fdfb3fc817b7ced4
flango <daniel.nagami@woopi.com.br>
2021-01-31 22:17:46 -0300

    Command and Command Handler

---
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Cliente.API.Application.Commands
+{
+    public class ClienteCommandHandler
+    {
+        public void Manipular(RegistrarClienteCommand message)
+        {
+            // Validar comando
+
+            // Persistir na base
+        }
+    }
+}
