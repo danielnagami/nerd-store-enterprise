85a79b8d9096b5b6907724c902c28c3552fa5665
Daniel Teixeira Nagami <daniel.nagami@gmail.com>
2021-03-14 01:24:02 -0300

    User refactoring

--- src/services/NSE.Carrinho.API/Controllers/CarrinhoController.cs
+++ src/services/NSE.Carrinho.API/Controllers/CarrinhoController.cs
@@ -1,11 +1,45 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Carrinho.API.Models;
 using NSE.WebAPI.Core.Controllers;
+using NSE.WebAPI.Core.Usuario;
+using System;
+using System.Threading.Tasks;
 
 namespace NSE.Carrinho.API.Controllers
 {
     [Authorize]
     public class CarrinhoController : MainController
     {
+        private readonly IAspNetUser _user;
 
+        public CarrinhoController(IAspNetUser user)
+        {
+            _user = user;
+        }
+
+        [HttpGet("carrinho")]
+        public async Task<CarrinhoCliente> ObterCarrinho()
+        {
+            return null;
+        }
+
+        [HttpGet("carrinho")]
+        public async Task<IActionResult> AdicionarItemCarrinho(CarrinhoItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpPut("carrinho/{produtoId}")]
+        public async Task<IActionResult> AtualizarItemCarrinho(Guid produtoId, CarrinhoItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete("carrinho/{produtoId}")]
+        public async Task<IActionResult> RemoverItemCarrinho(Guid produtoId)
+        {
+            return CustomResponse();
+        }
     }
 }
