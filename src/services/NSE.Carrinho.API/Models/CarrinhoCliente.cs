572fda8214ee48ef7c0f9336315cbd104c5462f3
Daniel Teixeira Nagami <daniel.nagami@gmail.com>
2021-03-14 23:51:12 -0300

    WIP: Handling API behavior

--- src/services/NSE.Carrinho.API/Models/CarrinhoCliente.cs
+++ src/services/NSE.Carrinho.API/Models/CarrinhoCliente.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NSE.Carrinho.API.Models
 {
     public class CarrinhoCliente
     {
+        internal const int MAX_QUANTIDADE_ITEM = 5;
         public CarrinhoCliente()
         {
 
@@ -20,5 +22,39 @@ namespace NSE.Carrinho.API.Models
         public Guid ClienteId { get; set; }
         public decimal ValorTotal { get; set; }
         public List<CarrinhoItem> Itens { get; set; } = new List<CarrinhoItem>();
+    
+        internal void CalcularValorCarrinho()
+        {
+            ValorTotal = Itens.Sum(p => p.CalcularValor());
+        }
+
+        internal bool CarrinhoItemExistente(CarrinhoItem item)
+        {
+            return Itens.Any(p => p.ProdutoId == item.ProdutoId);
+        }
+
+        internal CarrinhoItem ObterPorProdutoId(Guid produtoId)
+        {
+            return Itens.FirstOrDefault(p => p.ProdutoId == produtoId);
+        }
+
+        internal void AdicionarItem(CarrinhoItem item)
+        {
+            if (!item.EhValido()) return;
+
+            item.AssociarCarrinho(Id);
+
+            if (CarrinhoItemExistente(item))
+            {
+                var itemExistente = ObterPorProdutoId(item.ProdutoId);
+                itemExistente.AdicionarUnidades(item.Quantidade);
+
+                item = itemExistente;
+                Itens.Remove(itemExistente);
+            }
+
+            Itens.Add(item);
+            CalcularValorCarrinho();
+        }
     }
 }
