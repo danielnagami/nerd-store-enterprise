955044df711a71d7d95eda2bdb866e6332ec99f2
Daniel Teixeira Nagami <daniel.nagami@gmail.com>
2021-03-21 11:37:27 -0300

    WIP: Handling API behavior part.2

--- src/services/NSE.Carrinho.API/Models/CarrinhoItem.cs
+++ src/services/NSE.Carrinho.API/Models/CarrinhoItem.cs
@@ -34,6 +34,11 @@ namespace NSE.Carrinho.API.Models
             Quantidade += unidades;
         }
 
+        internal void AtualizarUnidades(int unidades)
+        {
+            Quantidade = unidades;
+        }
+
         internal bool EhValido()
         {
             return new ItemPedidoValidation().Validate(this).IsValid;
