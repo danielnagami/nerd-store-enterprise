d8361a61a8926fa4de35a0a167597e4ece373b88
Daniel Teixeira Nagami <daniel.nagami@gmail.com>
2021-03-26 00:25:34 -0300

    WIP: Consuming services from BFF

--- src/api gateways/NSE.BFF.Compras/Controllers/CarrinhoController.cs
+++ src/api gateways/NSE.BFF.Compras/Controllers/CarrinhoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NSE.BFF.Compras.Services;
 using NSE.WebAPI.Core.Controllers;
 using System.Threading.Tasks;
 
@@ -8,6 +9,15 @@ namespace NSE.BFF.Compras.Controllers
     [Authorize]
     public class CarrinhoController : MainController
     {
+        private readonly ICarrinhoService _carrinhoService;
+        private readonly ICatalogoService _catalogoService;
+
+        public CarrinhoController(ICarrinhoService carrinhoService, ICatalogoService catalogoService)
+        {
+            _carrinhoService = carrinhoService;
+            _catalogoService = catalogoService;
+        }
+
         [HttpGet]
         [Route("compras/carrinho")]
         public async Task<IActionResult> Index()
--- src/api gateways/NSE.BFF.Compras/Services/CarrinhoService.cs
+++ src/api gateways/NSE.BFF.Compras/Services/CarrinhoService.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.Extensions.Options;
 using NSE.BFF.Compras.Extensions;
+using NSE.BFF.Compras.Models;
+using NSE.Core.Communication;
 using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace NSE.BFF.Compras.Services
 {
-    public interface ICarrinhoService { }
-    public class CarrinhoService : ICarrinhoService
+    public interface ICarrinhoService 
+    {
+        Task<CarrinhoDTO> ObterCarrinho();
+        Task<ResponseResult> AdicionarItemCarrinho(ItemCarrinhoDTO produto);
+        Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoDTO carrinho);
+        Task<ResponseResult> RemoverItemCarrinho(Guid produtoId);
+    }
+    public class CarrinhoService : Service, ICarrinhoService
     {
         private readonly HttpClient _httpClient;
 
@@ -14,6 +23,46 @@ namespace NSE.BFF.Compras.Services
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.CarrinhoUrl);
+        }
+
+        public async Task<CarrinhoDTO> ObterCarrinho()
+        {
+            var response = await _httpClient.GetAsync("/carrinho/");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<CarrinhoDTO>(response);
+        }
+
+        public async Task<ResponseResult> AdicionarItemCarrinho(ItemCarrinhoDTO produto)
+        {
+            var itemContent = ObterConteudo(produto);
+
+            var response = await _httpClient.PostAsync("/carrinho/", itemContent);
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
+        }
+
+        public async Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoDTO carrinho)
+        {
+            var itemContent = ObterConteudo(carrinho);
+
+            var response = await _httpClient.PutAsync($"/carrinho/{carrinho.ProdutoId}", itemContent);
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
+        }
+
+        public async Task<ResponseResult> RemoverItemCarrinho(Guid produtoId)
+        {
+            var response = await _httpClient.DeleteAsync($"/carrinho/{produtoId}");
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
         }
     }
 }
